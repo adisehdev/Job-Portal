85146ea04a30ba74da8cd31f7a071e1b1024a456
adisehdev <sehdevaditya@gmail.com>
2024-10-04 15:00:36 +0530

    changes in setToken inside utils inside backend

--- backend/utils/setToken.js
+++ backend/utils/setToken.js
@@ -1,22 +1,21 @@
-export const setToken = (user,statusCode,res,message)=>{
-    
-    const token = user.createToken() //jwt sign method is called 
-    const options = {
-        expires: new Date(
-          Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
-        ),
-        httpOnly: true, // Set httpOnly to true
-        secure : true,
-        sameSite : "None",
-      }
-      
-      
-      res.status(statusCode).cookie("token", token, options).json({ //setting token inside cookies
-        success: true,
-        user,
-        message,
-        token,
-      });
+export const setToken = (user, statusCode, res, message) => {
+  const token = user.createToken(); //jwt sign method is called
 
-
-}
+  res
+    .status(statusCode)
+    .cookie("token", token, {
+      expires: new Date(
+        Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+      ),
+      httpOnly: true, // Set httpOnly to true
+      secure: true,
+      sameSite: "None",
+    })
+    .json({
+      //setting token inside cookies
+      success: true,
+      user,
+      message,
+      token,
+    });
+};
