7839a0c7cf5614fe73aecc2cd6f8d4c636d78db8
adisehdev <sehdevaditya@gmail.com>
2024-10-04 15:03:15 +0530

    undoing changes at setToken in utils in backend

--- backend/utils/setToken.js
+++ backend/utils/setToken.js
@@ -1,21 +1,22 @@
-export const setToken = (user, statusCode, res, message) => {
-  const token = user.createToken(); //jwt sign method is called
+export const setToken = (user,statusCode,res,message)=>{
+    
+    const token = user.createToken() //jwt sign method is called 
+    const options = {
+        expires: new Date(
+          Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+        ),
+        httpOnly: true, // Set httpOnly to true
+        secure : true,
+        sameSite : "None",
+      }
+      
+      
+      res.status(statusCode).cookie("token", token, options).json({ //setting token inside cookies
+        success: true,
+        user,
+        message,
+        token,
+      });
 
-  res
-    .status(statusCode)
-    .cookie("token", token, {
-      expires: new Date(
-        Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
-      ),
-      httpOnly: true, // Set httpOnly to true
-      secure: true,
-      sameSite: "None",
-    })
-    .json({
-      //setting token inside cookies
-      success: true,
-      user,
-      message,
-      token,
-    });
-};
+
+}
